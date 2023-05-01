cfb48a65b01f67e9050134ed16dfafa76e34ca95
AlvaroG-6 <83734390+AlvaroG-6@users.noreply.github.com>
2023-05-01 05:51:32 -0500

    Update data.js

--- demo01/data.js
+++ demo01/data.js
@@ -45,6 +45,23 @@ module.exports = function () {
         disponibilidad: "Disponible",
       }
     ],
+    
+    usuarios: [
+      {
+        id: 1,
+        nameUsuario: "Alvaro",
+        SnameUsuario: "Gamonal",
+        PassUsuario: "Odontologia",
+        telusuario: "990084583"
+      },
+      {
+        id: 2,
+        nameUsuario: "Gonzalo",
+        SnameUsuario: "Gamonal",
+        PassUsuario: "Emergencias",
+        telusuario: "988367020"
+      },
+    ]
    
   }
   return data
